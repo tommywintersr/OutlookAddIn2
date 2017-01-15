4bbde04d3ae804008d3faac71a2353ac9628d8ae
Tommy <Tommy@XPS13-1M0TL>
2017-01-14 16:04:42 -0800

    Add project files.

---
+++ OutlookAddIn2/ThisAddIn.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+
+namespace OutlookAddIn2
+{
+    public partial class ThisAddIn
+    {
+        Outlook.NameSpace outlookNameSpace;
+        Outlook.MAPIFolder inbox;
+        Outlook.Items items;
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            outlookNameSpace = this.Application.GetNamespace("MAPI");
+            inbox = outlookNameSpace.GetDefaultFolder(
+                    Microsoft.Office.Interop.Outlook.
+                    OlDefaultFolders.olFolderInbox);
+
+            items = inbox.Items;
+            items.ItemAdd +=
+                new Outlook.ItemsEvents_ItemAddEventHandler(items_ItemAdd);
+        }
+
+        void items_ItemAdd(object Item)
+        {
+
+            String filter = "http";
+
+            Outlook.MailItem mail = (Outlook.MailItem)Item;
+            if (Item != null)
+            {
+
+                    if (mail.MessageClass == "IPM.Note" &&
+                          mail.Body.ToUpper().Contains(filter.ToUpper()))
+                    {
+                        mail.Move(outlookNameSpace.GetDefaultFolder(
+                            Microsoft.Office.Interop.Outlook.
+                            OlDefaultFolders.olFolderJunk));
+                        
+                    }
+                
+               
+            }
+
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            // Note: Outlook no longer raises this event. If you have code that 
+            //    must run when Outlook shuts down, see http://go.microsoft.com/fwlink/?LinkId=506785
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
